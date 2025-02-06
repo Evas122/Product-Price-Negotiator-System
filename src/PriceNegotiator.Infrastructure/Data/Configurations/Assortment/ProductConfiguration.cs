76831caa52896ba516c8006aea9b1e0532780df1
Evas122 <108418453+Evas122@users.noreply.github.com>
2025-02-06 18:44:12 +0100

    entities for products with configurations and migrations

---
+++ src/PriceNegotiator.Infrastructure/Data/Configurations/Assortment/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PriceNegotiator.Domain.Entities.Assortment;
+
+namespace PriceNegotiator.Infrastructure.Data.Configurations.Assortment;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(p => p.BasePrice)
+            .HasColumnType("decimal(18,2)");
+    }
+}

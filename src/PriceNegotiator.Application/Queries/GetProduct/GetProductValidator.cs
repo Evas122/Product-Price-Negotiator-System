1f3403f15fd7cd1cc2bb53bf169076f6786821c6
Evas122 <108418453+Evas122@users.noreply.github.com>
2025-02-09 14:04:51 +0100

    added test project and create validator for get product and fix typo in controller annotations

---
+++ src/PriceNegotiator.Application/Queries/GetProduct/GetProductValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PriceNegotiator.Application.Queries.GetProduct;
+
+public class GetProductValidator : AbstractValidator<GetProductQuery>
+{
+    public GetProductValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Product Id cannot be empty.");
+    }
+}
